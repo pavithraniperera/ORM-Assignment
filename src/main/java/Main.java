22af7fd5d94347933c2122452dcad5027f4d6e82
pavithrani <pererapavithrani02@gmail.com>
2024-02-14 11:28:31 +0530

    Complete Quession-02

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,9 +14,9 @@ public class Main {
         Transaction transaction = session.beginTransaction();
 
         // Question -01
-        String hql = "From Book WHERE publicationYear > :year";
+        String hql1 = "From Book WHERE publicationYear > :year";
 
-        Query query = session.createQuery(hql);
+        Query query = session.createQuery(hql1);
         query.setParameter("year", Year.of(2010));
 
 
@@ -25,6 +25,14 @@ public class Main {
             System.out.println(book.getTitle());
         }
 
+        //Question-02
+        String hql2 ="Update Book SET price = price*1.1 WHERE author.id = :id ";
+        Query query1 = session.createQuery(hql2);
+        query1.setParameter("id",1);
+        int i = query1.executeUpdate();
+        if (i>0){
+            System.out.println("updated");
+        }
 
 
         transaction.commit();
